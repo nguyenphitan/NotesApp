8ffddb292bdc1a91ff17753b6cda2afedd5c8760
pvp100 <phamvietphong3001@gmail.com>
2023-05-25 22:20:58 +0700

    ss

--- Notes/app/src/main/java/com/npt/notes/NotesActivity.java
+++ Notes/app/src/main/java/com/npt/notes/NotesActivity.java
@@ -16,19 +16,23 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupMenu;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -162,6 +166,7 @@ public class NotesActivity extends AppCompatActivity {
                 return new NoteViewHolder(view);
             }
         };
+        noteAdapter.startListening();
 
 
         // Set layout to recycle
@@ -169,7 +174,6 @@ public class NotesActivity extends AppCompatActivity {
 
         // Set note adapter into recycle
         mRecycleView.setAdapter(noteAdapter);
-        noteAdapter.startListening();
 
     }
 
@@ -203,7 +207,50 @@ public class NotesActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
+        MenuItem item = menu.findItem(R.id.search);
+
+        SearchView searchView = (SearchView) item.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                onSearch(s);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                if (s.isEmpty()) {
+                    onSearch(s);
+                }
+                return false;
+            }
+        });
+
         return true;
+    }
+
+    private void onSearch(String keyword) {
+        Query query;
+        if (keyword.isEmpty()) {
+            query = firebaseFirestore.collection("notes")
+                    .document(firebaseUser.getUid())
+                    .collection("myNotes");
+        } else {
+            query = firebaseFirestore.collection("notes")
+                    .document(firebaseUser.getUid())
+                    .collection("myNotes").whereEqualTo("title", keyword);
+        }
+        query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        createNewNoteAdapter(query);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                    }
+                });
     }
 
     @Override
@@ -217,12 +264,106 @@ public class NotesActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private void createNewNoteAdapter(Query query) {
+        FirestoreRecyclerOptions<FirebaseModel> searchNotes = new FirestoreRecyclerOptions.Builder<FirebaseModel>()
+                .setQuery(query, FirebaseModel.class).build();
+        noteAdapter = new FirestoreRecyclerAdapter<FirebaseModel, NoteViewHolder>(searchNotes) {
+            @Override
+            protected void onBindViewHolder(@NonNull NoteViewHolder noteViewHolder, int position, @NonNull FirebaseModel firebaseModel) {
+                ImageView popupButton = noteViewHolder.itemView.findViewById(R.id.menupopbutton);
+
+                int colorCode = getRandomColor();
+                noteViewHolder.mNote.setBackgroundColor(noteViewHolder.itemView.getResources().getColor(colorCode,null));
+
+                noteViewHolder.noteTitle.setText(firebaseModel.getTitle());
+                noteViewHolder.noteContent.setText(firebaseModel.getContent());
+
+                String docId = noteAdapter.getSnapshots().getSnapshot(position).getId();
+
+                // Click get note detail
+                noteViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(v.getContext(), DetailActivity.class);
+                        intent.putExtra("title", firebaseModel.getTitle());
+                        intent.putExtra("content", firebaseModel.getContent());
+                        intent.putExtra("noteId", docId);
+                        v.getContext().startActivity(intent);
+
+                    }
+                });
+
+                // Click popup of note to edit or delete
+                popupButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        PopupMenu popupMenu = new PopupMenu(v.getContext(), v);
+                        popupMenu.setGravity(Gravity.END);
+
+                        // Add menu edit
+                        popupMenu.getMenu().add("Edit").setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                            @Override
+                            public boolean onMenuItemClick(@NonNull MenuItem menuItem) {
+                                Intent intent = new Intent(v.getContext(), EditNoteActivity.class);
+                                intent.putExtra("title", firebaseModel.getTitle());
+                                intent.putExtra("content", firebaseModel.getContent());
+                                intent.putExtra("noteId", docId);
+                                v.getContext().startActivity(intent);
+                                return false;
+                            }
+                        });
+
+                        // Add menu delete
+                        popupMenu.getMenu().add("Delete").setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                            @Override
+                            public boolean onMenuItemClick(@NonNull MenuItem menuItem) {
+
+                                // Select note by id
+                                DocumentReference documentReference = firebaseFirestore.collection("notes")
+                                        .document(firebaseUser.getUid())
+                                        .collection("myNotes")
+                                        .document(docId);
+
+                                // Delete note by id
+                                documentReference.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        Toast.makeText(v.getContext(), "This note is deleted", Toast.LENGTH_SHORT).show();
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(v.getContext(), "Fail to delete", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+
+                                return false;
+                            }
+                        });
+
+                        popupMenu.show();
+
+                    }
+                });
+
+            }
+
+            @NonNull
+            @Override
+            public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.notes_layout, parent, false);
+                return new NoteViewHolder(view);
+            }
+        };
+        noteAdapter.startListening();
+        mRecycleView.setAdapter(noteAdapter);
+    }
+
+
     @Override
-    protected void onStop() {
-        super.onStop();
-        if(noteAdapter != null) {
-            noteAdapter.stopListening();
-        }
+    protected void onDestroy() {
+        noteAdapter.stopListening();
+        super.onDestroy();
     }
 
     private int getRandomColor() {
